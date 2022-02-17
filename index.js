55fee8de1a368ca1f6663cdfbf474be85e076e9f
TaylorDarneille <tmdarneille@gmail.com>
2022-02-17 15:13:08 -0800

    started templates

--- index.js
+++ index.js
@@ -3,23 +3,33 @@ const express = require('express')
 // create an instance of express
 const app = express()
 
+// MIDDLEWARE
+// tell express to use ejs
+app.set('view engine', 'ejs')
+
+
 // ----------> ROUTES (aka ACTIONS) <----------
 
 // HOME
 app.get('/', (req, res)=>{
     console.log(__dirname)
     // __dirname = node shortcut for the absolute path to current directory
-    res.sendFile(__dirname+'/views/index.html')
+    // res.sendFile(__dirname+'/views/index.html')
     // res.send('This is the Home Page!')
+
+    // ejs already knows to look inside of a views folder
+    res.render('index.ejs', {name: 'Sterling Archer', age:35})
 })
 // ABOUT
 app.get('/about', (req, res)=>{
-    res.sendFile(__dirname+'/views/about.html')
+    // res.sendFile(__dirname+'/views/about.html')
     // res.send('Some stuff about me will go here.')
+    res.render('about.ejs')
 })
 // BLOG
 app.get('/blog', (req, res)=>{
-    res.sendFile(__dirname+'/views/blog-directory.html')
+    res.render('blog-directory.ejs')
+    // res.sendFile(__dirname+'/views/blog-directory.html')
     // res.send('A directory of all my blog posts will go here.')
 })
 
